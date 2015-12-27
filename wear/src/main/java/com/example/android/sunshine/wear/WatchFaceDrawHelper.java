35711738d0ee4acb1f203f5c6ba6fe26b11087ca
danijoo <danijob88@googlemail.com>
2015-12-27 21:25:12 +0100

    round style

--- wear/src/main/java/com/example/android/sunshine/wear/WatchFaceDrawHelper.java
+++ wear/src/main/java/com/example/android/sunshine/wear/WatchFaceDrawHelper.java
@@ -37,16 +37,11 @@ public class WatchFaceDrawHelper {
     float mTempLowXOffset;
 
     public WatchFaceDrawHelper(Resources resources, boolean isRound) {
-        mTimeYOffset = resources.getDimension(R.dimen.time_y_offset);
-        mDateYOffset = resources.getDimension(R.dimen.date_y_offset);
-        mTempYOffset = resources.getDimension(R.dimen.temp_y_offset);
-        mWeatherIconYOffset = resources.getDimension(R.dimen.weather_icon_y_offset);
 
         mBackgroundPaint = new Paint();
         mBackgroundPaint.setColor(resources.getColor(R.color.background));
 
         mIconPaint = new Paint();
-//        mIconPaint.setColor(resources.getColor(android.R.color.transparent));
 
         timeTextPaint = createTextPaint(resources.getColor(R.color.textcolor_primary));
         mDateTextPaint = createTextPaint(resources.getColor(R.color.textcolor_secondary));
@@ -54,6 +49,12 @@ public class WatchFaceDrawHelper {
         mHighestTempPaint = createTextPaint(resources.getColor(R.color.textcolor_primary));
         mHighestTempPaint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
         mLowestTempPaint = createTextPaint(resources.getColor(R.color.textcolor_primary));
+
+
+        mTimeYOffset = resources.getDimension(isRound? R.dimen.time_y_offset_round : R.dimen.time_y_offset);
+        mDateYOffset = resources.getDimension(isRound? R.dimen.date_y_offset_round : R.dimen.date_y_offset);
+        mTempYOffset = resources.getDimension(isRound? R.dimen.temp_y_offset_round : R.dimen.temp_y_offset);
+        mWeatherIconYOffset = resources.getDimension(isRound? R.dimen.weather_icon_y_offset_round : R.dimen.weather_icon_y_offset);
 
         mTimeXOffset = resources.getDimension(isRound
                 ? R.dimen.time_x_offset_round : R.dimen.time_x_offset);
@@ -75,11 +76,11 @@ public class WatchFaceDrawHelper {
         mDateTextPaint.setTextSize(dateTextSize);
 
         float weatherTextSize = resources.getDimension(isRound
-                ? R.dimen.weather_text_size_round : R.dimen.weather_text_size);
+                ? R.dimen.temp_high_size_round : R.dimen.temp_high_text_size);
         mHighestTempPaint.setTextSize(weatherTextSize);
 
         float weatherTextSize2 = resources.getDimension(isRound
-                ? R.dimen.weather_text_size_round_2 : R.dimen.weather_text_size_2);
+                ? R.dimen.temp_low_size_round : R.dimen.temp_low_size);
         mLowestTempPaint.setTextSize(weatherTextSize2);
     }
 
@@ -121,12 +122,12 @@ public class WatchFaceDrawHelper {
         canvas.drawText(dateText, mDateXOffset, mDateYOffset, mDateTextPaint);
 
         // Draw weather
-        if(!(highestTemp == 0 && lowestTemp == 0)) {
+//        if(!(highestTemp == 0 && lowestTemp == 0)) {
             String currentHigh = String.format(TEMP_FORMAT, highestTemp);
             canvas.drawText(currentHigh, mTempHighXOffset, mTempYOffset, mHighestTempPaint);
             String currentLow = String.format(TEMP_FORMAT, lowestTemp);
             canvas.drawText(currentLow, mTempLowXOffset, mTempYOffset, mLowestTempPaint);
-        }
+//        }
 
         if(weatherIcon != null)
             canvas.drawBitmap(weatherIcon, mWeatherIconXOffset, mWeatherIconYOffset, mIconPaint);
